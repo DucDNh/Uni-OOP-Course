ca93f56f8180c3e71ae160ddfdec3b7fa6216127
DucDNh <linhnguyen.280459@gmail.com>
2026-03-03 00:03:42 +0700

    Update Exercise 3b

--- Chuong 1 Bai Tap 3b/Chuong 1 Bai Tap 3b/3b.cpp
+++ Chuong 1 Bai Tap 3b/Chuong 1 Bai Tap 3b/3b.cpp
@@ -4,5 +4,31 @@ using namespace std;
 
 class PhanSo {
 	int tu, mau;
-
+public:
+	void nhap() {
+		char gachcheo;
+		cout << "Nhap phan so : ";
+		cin >> tu >> gachcheo >> mau;
+	}
+	void xuat() {
+		if (mau == 1)
+			cout << tu << endl;
+		else
+			cout << tu << '/' << mau << endl;
+	}
+	bool operator> (PhanSo tem_ps) {
+		int tem_tu1 = tu * tem_ps.mau, tem_tu2 = tem_ps.tu * mau;
+		return (tem_tu1 > tem_tu2);
+	}
 };
+
+int main() {
+	PhanSo ps1, ps2;
+	ps1.nhap();
+	ps2.nhap();
+	if (ps1 > ps2)
+		ps1.xuat();
+	else
+		ps2.xuat();
+	return 0;
+}
